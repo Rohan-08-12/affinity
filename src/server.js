1506e26290afbee4c6d1568fb643601e787500af
Rohan <rohannayyer12@gmail.com>
2026-01-22 14:36:51 -0500

    Update CORS to allow Netlify preview URLs

--- src/server.js
+++ src/server.js
@@ -16,9 +16,16 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
+  
+  // Check if origin is in allowed list
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+  } 
+  // Allow Netlify preview URLs (format: https://hash--site.netlify.app)
+  else if (origin && origin.includes('.netlify.app')) {
+    res.header('Access-Control-Allow-Origin', origin);
   }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
